6d8a6331258313681fc4cb1cfea1baeb17002d79
elhombretecla <delacruzgarciajuan@gmail.com>
2022-07-22 10:45:55 +0200

    WIP

--- client/src/routes/HomeSessions.svelte
+++ client/src/routes/HomeSessions.svelte
@@ -13,14 +13,16 @@
   <h1>Session</h1>
   <img src="{img}" alt="bows">
   <Button type="secondary" disabled={true}>join session</Button>
-  <Button
-    on:click|once={navigateToNewSession}>
-    create session
-  </Button>
-  <Button
-    on:click|once={() => navigate("/sessions/annotations/0")}>
-    go to sesion
-  </Button>
+  <div class="btn-row">
+    <Button type="secondary"
+      on:click|once={() => navigate("/sessions/annotations/0")}>
+      go to sesion
+    </Button>
+    <Button
+      on:click|once={navigateToNewSession}>
+      create session
+    </Button>
+  </div>
 </div>
 
 
@@ -37,6 +39,14 @@
 
   & h1 {
     font-size: 2rem;
+  }
+  
+  & .btn-row {
+    border-top: 1px solid var(--color-gray-light);
+    display: flex;
+    width: 100%;
+    gap: 1rem;
+    padding-top: 1rem;
   }
 }
 

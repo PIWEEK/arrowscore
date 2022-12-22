647b6d8fb8367577900685a976ca1cc58b5198f3
diacritica <diacritica@gmail.com>
2022-12-22 13:23:38 +0100

    new files for tournamentsessions

---
+++ client/src/components/ScoreSystemDownloadOrCreate.svelte
@@ -0,0 +1,107 @@
+<script>
+    import { navigate } from "svelte-navigator";
+    import { apiClient } from "../services/api"
+    import { localStorage } from "../services/storages"
+    import Button from './Button.svelte'
+  
+  
+    const navigateToNewScoreSystem = () => {
+      navigate("/score-systems/new")
+    }
+  
+  
+    let code = null
+  
+    const downloadServerScoreSystem = async () => {
+      const {data} = await apiClient(
+        "GET",
+        "score-systems?populate=*&filters[code][$eq]="+code,
+      )
+      const localScoreSystems = localStorage.get("scoreSystems") || []
+      console.log("downloaded SS"+data[0].attributes.name)
+  
+      let existingLocalScoreSystems = []
+      
+      for (const i of localScoreSystems){
+        existingLocalScoreSystems.push(i.attributes["code"])
+      }
+      
+      if (! existingLocalScoreSystems.includes(data[0].attributes.code)){
+          localScoreSystems.push(data[0])
+          console.log("YEAH!")
+        }
+      else {
+          console.log("Dup!")
+      }
+      localStorage.set("scoreSystems", localScoreSystems)
+  
+      
+  
+    }
+  
+  </script>
+  
+  
+   <div class="btn-row">
+    <form on:submit|preventDefault={downloadServerScoreSystem}>
+      <input
+      type="text"
+      name="code"
+      placeholder="Enter code" 
+      bind:value={code} />
+      <Button
+        theme="secondary"
+        on:click|once={downloadServerScoreSystem}
+        disabled={false}>
+        download
+      </Button>
+    </form>
+    </div>
+    <!-- <Button
+      theme="secondary"
+      on:click|once={navigateToNewAdvanceScoreSystem}
+      disabled={true}>
+      create advanced
+    </Button>-->
+    <Button
+      on:click|once={navigateToNewScoreSystem}>
+      create score system
+    </Button>
+  
+  
+  
+    <style lang="postcss">
+    form {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            gap: 1.4rem;
+            width: 100%;
+        
+            & .field {
+              display: flex;
+              flex-direction: column;
+              gap: .4rem;
+              width: 100%;
+        
+              & label {
+                font-family: 'Manrope', serif;
+                font-weight: 600;
+                font-size: 1.1rem;
+                padding-left: .5rem;
+              }
+            }
+          }
+
+           .btn-row {
+            border-top: 0px solid var(--color-gray-light);
+            display: flex;
+            width: 100%;
+            gap: 1rem;
+            padding-top: 1rem;
+          }
+        
+
+        </style>
+        
---
+++ client/src/routes/HomeTournamentSessions.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { navigate } from "svelte-navigator";
+    import Button from '../components/Button.svelte'
+    import img from '../assets/imgs/img-tournamentsession.png'
+  
+    const navigateToNewTournamentSession = () => {
+      navigate("/tournamentsessions/new")
+    }
+  
+  </script>
+  
+  <div class="sessions">
+    <h1>Tournament Session</h1>
+    <img src="{img}" alt="bows">
+    <Button theme="secondary"  on:click={() => { navigate("/my/tournamentsessions")}} disabled={false}>
+      rejoin tournament session
+    </Button>
+    <Button
+      on:click|once={navigateToNewTournamentSession}>
+      create tournament session
+    </Button>
+  </div>
+  
+  
+  <style lang="postcss">
+  
+  .sessions{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    height: 100%;
+    padding: 5vh 0;
+  
+    & h1 {
+      font-size: 2rem;
+    }
+  }
+  
+  img {
+    flex-grow: 1;
+    height: 200px;
+  }
+  </style>
+  
